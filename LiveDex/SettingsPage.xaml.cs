40e72f37f4c5a631cba45780680772475487437d
Tayler Uva <tayleruva@gmail.com>
2019-04-14 13:05:55 -0700

    Update SettingsPage.xaml.cs
    added user alerts

--- LiveDex/SettingsPage.xaml.cs
+++ LiveDex/SettingsPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace LiveDex {
@@ -9,16 +9,30 @@ namespace LiveDex {
             InitializeComponent();
         }
 
+        const string youAreAbout = "You are about to change the caught status of ALL pokemon to:\n";
+        const string noUndo = "\nThis CANNOT be undone!";
+        const string statusSet = "The caught status of ALL pokemon have changed to:\n";
+
         async void SetAllPokemonMissing(object sender, System.EventArgs e) {
-            loading.IsRunning = true;
-            await App.CaughtDatabaseInstance.PopulateDatabase(false, true);
-            loading.IsRunning = false;
+            await SetAllCaughtStatus(false);
         }
 
         async void SetAllPokemonCaught(object sender, System.EventArgs e) {
-            loading.IsRunning = true;
-            await App.CaughtDatabaseInstance.PopulateDatabase(true, true);
-            loading.IsRunning = false;
+            await SetAllCaughtStatus(true);
+        }
+
+        async Task SetAllCaughtStatus(bool caught) {
+            string caughtString = caught.ToString().ToUpper();
+            var changeSelected = await DisplayAlert("Are you sure?",
+                  youAreAbout + caughtString + noUndo,
+                  "Change",
+                  "Cancel");
+            if (changeSelected) {
+                loading.IsRunning = true;
+                await App.CaughtDatabaseInstance.PopulateDatabase(false, true);
+                loading.IsRunning = false;
+                await DisplayAlert("Changed!", statusSet + caughtString, "Okay");
+            }
         }
     }
 }
