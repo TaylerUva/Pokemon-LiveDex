85a9147118adce81b83c5f52405997373a8d4c08
AngelaKimmy <angelakim.la@gmail.com>
2019-04-25 14:08:45 -0700

    added trigger

---
+++ LiveDex/Models/PokedexTriggers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace LiveDex.Models{
+
+    public class PokedexBehavior : TriggerAction<Button>{
+        protected override void Invoke(Button sender){
+            //throw new NotImplementedException();
+            if (!PokeData.AllCaught()){
+                sender.Text = "YOU GOT ALL THE POKEMON";
+                sender.IsEnabled = false;
+            }
+        }
+    }
+
+    public class ReleaseAllTrigger : TriggerAction<Button>{
+        protected override void Invoke(Button sender){
+            //throw new NotImplementedException();
+            if (!PokeData.AllMissing()){
+                sender.Text = "ALL THE POKEMON ARE GONE";
+                sender.IsEnabled = false;
+            }
+
+        }
+    }
+}
