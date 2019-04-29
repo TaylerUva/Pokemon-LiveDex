18c895a2b5ee7f5cdd20d4bea986af80f8132ddd
AngelaKimmy <angelakim.la@gmail.com>
2019-04-28 19:16:36 -0700

    Starting resources and styles

--- LiveDex/Models/PokedexTriggers.cs
+++ LiveDex/Models/PokedexTriggers.cs
@@ -1,8 +1,56 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using Xamarin.Forms;
 
 namespace LiveDex.Models{
+    /*
+    public class SettingsAppear : Trigger
+    {
+        protected override void Invoke(ContentPage sender)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    */
+    /*
+    public class MultiTriggerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if ((int)value > 0) // length > 0 ?
+                return true;            // some data has been entered
+            else
+                return false;            // input is empty
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+    */
+
+    public class MultiTriggerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if (!PokeData.AllCaught()) // length > 0 ?
+                return true;            // some data has been entered
+            else
+                return false;            // input is empty
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
 
     public class PokedexBehavior : TriggerAction<Button>{
         protected override void Invoke(Button sender){
