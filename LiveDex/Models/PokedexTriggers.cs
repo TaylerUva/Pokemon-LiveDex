ecf704a633eeed3c0f4c4809a2befa76cc77cd83
Tayler Uva <tayleruva@gmail.com>
2019-05-02 12:17:19 -0700

    changed label on main page

--- LiveDex/Models/PokedexTriggers.cs
+++ LiveDex/Models/PokedexTriggers.cs
@@ -5,7 +5,7 @@ using Xamarin.Forms;
 using System.Reflection;
 using System.Windows.Input;
 
-namespace LiveDex.Models{
+namespace LiveDex.Models {
     /*
     public class SettingsAppear : Trigger
     {
@@ -52,15 +52,14 @@ namespace LiveDex.Models{
     //        throw new NotSupportedException();
     //    }
     //}
-    public class PickerBehavior : Behavior<Picker>
-    {
+    public class PickerBehavior : Behavior<Picker> {
         static readonly BindableProperty blueGenProperties = BindableProperty.Create(nameof(blueGens), typeof(string[]), typeof(PickerBehavior));
 
-        public string[] blueGens = {"blue", "diamond", "sapphire", "x" };
-        public string[] redGens = {"firered", "ruby", "red", "y" };
-        public string[] greenGens = { "leafgreen", "emerald"};
+        public string[] blueGens = { "blue", "diamond", "sapphire", "x" };
+        public string[] redGens = { "firered", "ruby", "red", "y" };
+        public string[] greenGens = { "leafgreen", "emerald" };
         public string[] silverGens = { "platinum", "soulsilver" };
-        public string[] blackGens = {"black", "black 2" };
+        public string[] blackGens = { "black", "black 2" };
         public string[] whiteGens = { "white", "white 2" };
         //public string[] blueGens
         //{
@@ -69,66 +68,47 @@ namespace LiveDex.Models{
         //}
 
 
-        protected override void OnAttachedTo(Picker bindable)
-        {
+        protected override void OnAttachedTo(Picker bindable) {
             //base.OnAttachedTo(bindable);
             bindable.SelectedIndexChanged += Bindable_SelectedIndexChanged;
         }
-        protected override void OnDetachingFrom(Picker bindable)
-        {
+        protected override void OnDetachingFrom(Picker bindable) {
             //base.OnDetachingFrom(bindable);
             bindable.SelectedIndexChanged -= Bindable_SelectedIndexChanged;
         }
 
-        void Bindable_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (blueGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower()))
-            {
-                ((Picker)sender).BackgroundColor = Color.Blue;
+        void Bindable_SelectedIndexChanged(object sender, EventArgs e) {
+            if (blueGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower())) {
+                ((Picker)sender).BackgroundColor = Color.FromHex("2B206F");
                 ((Picker)sender).TextColor = Color.White;
-            }
-            else if (redGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower()))
-            {
+            } else if (redGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower())) {
                 ((Picker)sender).BackgroundColor = Color.Red;
                 ((Picker)sender).TextColor = Color.White;
-            }
-            else if (greenGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower()))
-            {
+            } else if (greenGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower())) {
                 ((Picker)sender).BackgroundColor = Color.Green;
                 ((Picker)sender).TextColor = Color.White;
-            }
-            else if (silverGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower()))
-            {
+            } else if (silverGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower())) {
                 ((Picker)sender).BackgroundColor = Color.Silver;
                 ((Picker)sender).TextColor = Color.White;
-            }
-            else if (((Picker)sender).SelectedItem.ToString().ToLower()=="heartgold")
-            {
+            } else if (((Picker)sender).SelectedItem.ToString().ToLower() == "heartgold") {
                 ((Picker)sender).BackgroundColor = Color.Goldenrod;
                 ((Picker)sender).TextColor = Color.White;
-            }
-            else if (((Picker)sender).SelectedItem.ToString().ToLower() == "yellow")
-            {
+            } else if (((Picker)sender).SelectedItem.ToString().ToLower() == "yellow") {
                 ((Picker)sender).BackgroundColor = Color.Gold;
                 ((Picker)sender).TextColor = Color.White;
-            }
-            else if (((Picker)sender).SelectedItem.ToString().ToLower() == "pearl")
-            {
+            } else if (((Picker)sender).SelectedItem.ToString().ToLower() == "pearl") {
                 ((Picker)sender).BackgroundColor = Color.DeepPink;
                 ((Picker)sender).TextColor = Color.White;
-            }
-            else if (blackGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower()))
-            {
+            } else if (blackGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower())) {
                 ((Picker)sender).BackgroundColor = Color.Black;
                 ((Picker)sender).TextColor = Color.White;
             }
-            //else if (whiteGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower()))
-            //{
-            //    ((Picker)sender).BackgroundColor = Color.Default;
-            //    ((Picker)sender).TextColor = Color.White;
-            //}
-            else
-            {
+              //else if (whiteGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower()))
+              //{
+              //    ((Picker)sender).BackgroundColor = Color.Default;
+              //    ((Picker)sender).TextColor = Color.White;
+              //}
+              else {
                 ((Picker)sender).BackgroundColor = Color.Default;
                 ((Picker)sender).TextColor = Color.Default;
 
@@ -137,22 +117,22 @@ namespace LiveDex.Models{
         }
     }
 
-    
 
-    public class CatchAllTrigger : TriggerAction<Button>{
-        protected override void Invoke(Button sender){
+
+    public class CatchAllTrigger : TriggerAction<Button> {
+        protected override void Invoke(Button sender) {
             //throw new NotImplementedException();
-            if (!PokeData.AllCaught()){
+            if (!PokeData.AllCaught()) {
                 sender.Text = "You got 'em all!";
                 sender.IsEnabled = false;
             }
         }
     }
 
-    public class ReleaseAllTrigger : TriggerAction<Button>{
-        protected override void Invoke(Button sender){
+    public class ReleaseAllTrigger : TriggerAction<Button> {
+        protected override void Invoke(Button sender) {
             //throw new NotImplementedException();
-            if (!PokeData.AllMissing()){
+            if (!PokeData.AllMissing()) {
                 sender.Text = "Better catch 'em all!";
                 sender.IsEnabled = false;
             }
