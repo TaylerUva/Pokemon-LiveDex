054ba4ee9d21df011856f6480a8a49d94be03bb8
AngelaKimmy <angelakim.la@gmail.com>
2019-05-02 01:43:12 -0700

    Added picker behavior

--- LiveDex/Models/PokedexTriggers.cs
+++ LiveDex/Models/PokedexTriggers.cs
@@ -2,6 +2,8 @@
 using System.Globalization;
 using System.Linq;
 using Xamarin.Forms;
+using System.Reflection;
+using System.Windows.Input;
 
 namespace LiveDex.Models{
     /*
@@ -50,7 +52,73 @@ namespace LiveDex.Models{
     //        throw new NotSupportedException();
     //    }
     //}
+    public class PickerBehavior : Behavior<Picker>
+    {
+        static readonly BindableProperty blueGenProperties = BindableProperty.Create(nameof(blueGens), typeof(string[]), typeof(PickerBehavior));
 
+        public string[] blueGens = {"blue", "diamond", "x" };
+        //public string[] blueGens
+        //{
+        //    get => (string[])GetValue(blueGenProperties);
+        //    set => SetValue(blueGenProperties, value);
+        //}
+
+
+        protected override void OnAttachedTo(Picker bindable)
+        {
+            //base.OnAttachedTo(bindable);
+            bindable.SelectedIndexChanged += Bindable_SelectedIndexChanged;
+        }
+        protected override void OnDetachingFrom(Picker bindable)
+        {
+            //base.OnDetachingFrom(bindable);
+            bindable.SelectedIndexChanged -= Bindable_SelectedIndexChanged;
+        }
+
+        void Bindable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if(!(sender is Picker bindable)) { return; }
+            //if(!(bindable.ItemDisplayBinding is Binding displayBiding)) { return; }
+
+            //var displayBindingPath = displayBiding.Path;
+
+            //var selectedItem = bindable.SelectedItem.GetType().GetRuntimeProperty(displayBindingPath);
+            //var selectedText = selectedItem.GetValue(bindable.SelectedItem);
+
+
+            ////((Picker)sender).SelectedItem.T
+
+            ////((Picker)sender).ItemsSource.
+            //if (blueGens.Contains(selectedText))
+            //{
+            //    bindable.BackgroundColor = Color.Blue;
+            //}
+
+            //if (blueGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower()))
+            //{
+            //    bindable.BackgroundColor = Color.Blue;
+            //    ((Picker)sender).BackgroundColor = Color.Blue;
+            //}
+
+            if (blueGens.Contains(((Picker)sender).SelectedItem.ToString().ToLower()))
+            {
+                ((Picker)sender).BackgroundColor = Color.Blue;
+                ((Picker)sender).TextColor = Color.White;
+            }
+            else
+            {
+                ((Picker)sender).BackgroundColor = Color.Default;
+                ((Picker)sender).TextColor = Color.Default;
+
+            }
+            //if (selectedText.ToString() == "Blue")
+            //{
+            //    ((Picker)sender).BackgroundColor = Color.Blue;
+            //}
+        }
+    }
+
+    
 
     public class CatchAllTrigger : TriggerAction<Button>{
         protected override void Invoke(Button sender){
